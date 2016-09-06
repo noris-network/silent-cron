79ec3544a55278472d91a1604d7c3ddff0994112
Moritz Lenz <moritz@faui2k3.org>
2016-09-06 09:14:23 +0200

    Get basic functionality in place

--- lib/SilentCron/Storage/SQLite.pm
+++ lib/SilentCron/Storage/SQLite.pm
@@ -7,6 +7,8 @@ use 5.014;
 use DBI;
 use Params::Validate qw(validate);
 
+use SilentCron::JobRun;
+
 use constant TABLE => 'job_run';
 use constant DATETIME_FORMAT => '%Y-%m-%d %H:%M:%S';
 
@@ -88,6 +90,54 @@ sub record {
         $run->end_time->strftime(DATETIME_FORMAT),
    );
    $run->id( $dbh->sqlite_last_insert_rowid );
+}
+
+sub list {
+    my $self = shift;
+    validate(@_, {
+        job_name    => 0,
+        limit       => 0,
+    });
+    my %opts = @_;
+    my @sql_where;
+    my @bind_params;
+
+    if ( defined $opts{job_name} ) {
+        push @sql_where, "job_name = ?";
+        push @bind_params, $opts{job_name};
+    }
+
+    unless ( @sql_where ) {
+        @sql_where = '1 = 1';
+    }
+
+    my $where = join ' AND ', @sql_where;
+
+    my $sql = <<SQL;
+        SELECT id, job_name, exit_code, output, start_time, end_time
+        FROM   ${\TABLE}
+        WHERE  $where
+        ORDER BY  end_time DESC
+SQL
+    if ( $opts{limit} ) {
+       $sql .= " LIMIT $opts{limit}";
+    }
+
+    my $sth = $self->_dbh->prepare($sql);
+    $sth->execute(@bind_params);
+    $sth->bind_columns(\my ($id, $job_name, $exit_code, $output, $start_time, $end_time));
+    my @result;
+    while ($sth->fetch) {
+        push @result, SilentCron::JobRun->new(
+            id => $id,
+            job_name    => $job_name,
+            exit_code   => $exit_code,
+            output      => $output,
+            start_time  => Time::Piece->strptime($start_time, DATETIME_FORMAT),
+            end_time    => Time::Piece->strptime($end_time, DATETIME_FORMAT),
+        );
+    }
+    return @result;
 }
 
 
